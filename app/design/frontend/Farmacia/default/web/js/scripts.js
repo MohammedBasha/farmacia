61553e292f29b4dbf652ec3aa3ba926d71aa7486
MohammedBasha <me@mohammedbasha.com>
2021-05-09 15:50:23 +0200

    Adding the Structure and Styles for the Heeader, search, minicart and banner

--- app/design/frontend/Farmacia/default/web/js/scripts.js
+++ app/design/frontend/Farmacia/default/web/js/scripts.js
@@ -1,7 +1,16 @@
 require(['jquery', "slick"], function($){
     $(document).ready( function() {
 
-        
+        // Adding title attribute to the my-acount link
+        $(".my-account-link").attr("title","accedi/registrati");
+
+        // Init the Banner slider with Slick lib
+        $('.home-banner-wrapper').slick({
+            dots: true,
+            centerMode: true,
+            centerPadding: '120px',
+            slidesToShow: 1
+        });
 
     });
 });
