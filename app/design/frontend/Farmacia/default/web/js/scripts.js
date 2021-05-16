9309941723d920f0a3edf400040e63e3c2be609c
MohammedBasha <me@mohammedbasha.com>
2021-05-16 09:21:44 +0200

    Adding the Responsive to the Header section

--- app/design/frontend/Farmacia/default/web/js/scripts.js
+++ app/design/frontend/Farmacia/default/web/js/scripts.js
@@ -5,32 +5,67 @@ require(['jquery', "slick", "mage/mage"], function($){
         $(".my-account-link").attr("title","accedi/registrati");
 
         // Initializing the Sticky widget to the header
-        $('.header.content').mage('sticky', {
-            container: '.page-header'
-        });
+        if ($(window).width() >= 767) {
+            $('.header.content').mage('sticky', {
+                container: '.page-header'
+            });
+        }
 
         // Adding the Sticky function to the Main Navigation
-        $('.sticky-nav-toggle i').click(function() {
-            if($('.nav-sections').hasClass('_sticky')) {
-                $('.nav-sections').removeClass('_sticky');
-            } else {
-                $('.nav-sections').addClass('_sticky');
-                $('.nav-sections._sticky').css('top', $('.header.content').outerHeight());
+        if ($(window).width() >= 767) {
+            $('.sticky-nav-toggle i').click(function() {
+                if($('.nav-sections').hasClass('_sticky')) {
+                    $('.nav-sections').removeClass('_sticky');
+                } else {
+                    $('.nav-sections').addClass('_sticky');
+                    $('.nav-sections._sticky').css('top', $('.header.content').outerHeight());
+                }
+            });
+
+            setInterval(function(){
+                if(!$('.sticky-nav-toggle').is(':visible')){
+                    $('.nav-sections').removeClass('_sticky');
+                }
+            }, 200);
+        }
+
+        $(window).on('resize', function(){
+            if ($(window).width() >= 767) {
+                $('.sticky-nav-toggle i').click(function() {
+                    if($('.nav-sections').hasClass('_sticky')) {
+                        $('.nav-sections').removeClass('_sticky');
+                    } else {
+                        $('.nav-sections').addClass('_sticky');
+                        $('.nav-sections._sticky').css('top', $('.header.content').outerHeight());
+                    }
+                });
+
+                setInterval(function(){
+                    if(!$('.sticky-nav-toggle').is(':visible')){
+                        $('.nav-sections').removeClass('_sticky');
+                    }
+                }, 200);
             }
         });
-
-        setInterval(function(){
-            if(!$('.sticky-nav-toggle').is(':visible')){
-                $('.nav-sections').removeClass('_sticky');
-            }
-        }, 200);
 
         // Init the Banner slider with Slick lib
         $('.home-banner-wrapper').slick({
             dots: true,
             centerMode: true,
             centerPadding: '120px',
-            slidesToShow: 1
+            slidesToShow: 1,
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true,
+                        centerPadding: '120px'
+                    }
+                }
+            ]
         });
 
     });
